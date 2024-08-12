b69c15f3627842e15378dd05189b3fa2b825dd98
Rehan <rehanalire52@gmail.com>
2024-08-12 21:26:16 +0530

    add signup validation with zod

---
+++ validation/auth.validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const signupValidation = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters" })
+    .max(20, { message: "Username must be less than 20 characters" })
+    .trim()
+    .regex(/^[a-zA-Z0-9_-]+$/, {
+      message: "Username cannot contain special characters",
+    }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" })
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+      {
+        message:
+          "Password must include uppercase, lowercase, number, and special character",
+      }
+    ),
+});
