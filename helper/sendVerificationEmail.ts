f078b96b0c204df6f8db4c7855121c00918a859a
Rehan <rehanalire52@gmail.com>
2024-08-15 20:45:05 +0530

    add sendVerificationEmail function to code base

---
+++ helper/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../emails/VerificationEmail";
+import { NextResponse } from "next/server";
+
+export const sendVerificationEmail = async (
+  email: string,
+  username: string,
+  verifyCode: string
+) => {
+  try {
+    const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: email,
+        subject: "verificationCode",
+        react: VerificationEmail({username: username, otp: verifyCode}),
+      });
+  
+  } catch (error) {
+    console.error("Failed to send verification email:", error);
+    return NextResponse.json({
+      success: false,
+      error: "Failed to send verification email",
+    });
+  }
+};
