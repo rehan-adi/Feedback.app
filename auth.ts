f013f18a598f1aac449c4e415c1d59f1407d46f7
Rehan <rehanalire52@gmail.com>
2024-08-23 16:03:49 +0530

    Configure NextAuth with GitHub provider, Prisma adapter, and JWT sessions.

--- auth.ts
+++ auth.ts
@@ -1,5 +1,32 @@
 import NextAuth from "next-auth"
+import prisma from "./lib/prisma"
+import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.sub as string;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
 })
