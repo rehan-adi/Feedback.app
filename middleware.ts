b0b84b3951a0beff185fd8d1282a2f7f11ffa2b2
Rehan <rehanalire52@gmail.com>
2024-08-28 15:40:17 +0530

    Add middleware to code base

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  const isPublicPath = path === '/signin' || path === '/signup' || path === '/verify-email'
+
+  const token = request.cookies.get('token')?.value || ''
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/signin', request.nextUrl))
+  }
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/signup',
+    '/verify-email',
+    '/signin',
+    '/dashboard',
+  ]
+}
