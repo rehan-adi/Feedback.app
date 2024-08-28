ba2816cdbebab24a45b957c51982a0768cbf083c
Rehan <rehanalire52@gmail.com>
2024-08-28 14:10:03 +0530

    Add Footer

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,11 @@
+import Footer from '@/components/dashboard/Footer'
 import Navbar from '@/components/dashboard/Navbar'
 
 const layout = () => {
   return (
     <div>
        <Navbar />
+       <Footer />
     </div>
   )
 }
---
+++ components/dashboard/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="text-center fixed text-sm font-medium  bottom-0 w-full p-4 md:p-6 bg-black text-white">
+       © 2024 True Feedback. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
