bb0aa439ca840bbd2df63a9a578d0d9812abdb34
Rehan <rehanalire52@gmail.com>
2024-08-28 15:11:48 +0530

    Removed loading state and added DropdownMenu and avtar from shadcn

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -2,17 +2,24 @@
 
 import axios from "axios";
 import { toast } from "sonner";
-import { useState } from "react";
-import { Button } from "../ui/button";
-import { Loader2 } from "lucide-react";
+import { Github, LogOut, Settings, User } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
 
 const Navbar = () => {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
 
   const onLogout = async () => {
-    setLoading(true);
     try {
       const response = await axios.post("/api/logout");
       if (response.status === 200) {
@@ -26,27 +33,52 @@ const Navbar = () => {
       console.error(error);
       toast.error("Logout failed", {
         description:
-          error?.response?.data?.message || "An error occurred. Please try again.",
+          error?.response?.data?.message ||
+          "An error occurred. Please try again.",
         duration: 3000,
       });
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
-    <div className="bg-black flex justify-between items-center lg:px-20 px-6 text-white h-20 w-full ">
+    <div className="bg-black flex justify-between items-center lg:px-24 px-6 text-white h-20 w-full ">
       <h1>Logo</h1>
-
-      <Button variant="secondary" onClick={onLogout} disabled={loading}>
-        {loading ? (
-          <>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Logging out...
-          </>
-        ) : (
-          "Logout"
-        )}
-      </Button>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-52 mr-14 dark">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Github className="mr-2 h-4 w-4" />
+                <span>GitHub</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={onLogout}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Log out</span>
+              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
