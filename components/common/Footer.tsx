5cc55ebef2a7d21a047ecb05787f9d9dce18fd34
Rehan <rehanalire52@gmail.com>
2024-08-28 18:45:42 +0530

    Make the Navbar fixed and add border bottom

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="text-center fixed text-sm font-medium  bottom-0 w-full p-4 md:p-6 bg-black text-white">
+    <footer className="text-center text-sm font-medium w-full p-4 md:p-6 bg-black text-white">
        © 2024 True Feedback. All rights reserved.
     </footer>
   )
