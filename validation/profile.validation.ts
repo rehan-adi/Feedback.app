601cd0adf0a1f655dd58dc081a96e86680ab8f93
Rehan <rehanalire52@gmail.com>
2024-08-29 22:58:51 +0530

    Change emailVerified to isVerified in profile validation

--- validation/profile.validation.ts
+++ validation/profile.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const profileValidation = z.object({
   username: z.string(),
   email: z.string().email(),
-  emailVerified: z.boolean(),
+  isVerified: z.boolean(),
 });
 
 export type profileData = z.infer<typeof profileValidation>;
