a8a6ce36824ed10cb12fbcf59c4e5fadcff13d39
Rehan <rehanalire52@gmail.com>
2024-09-09 19:24:08 +0530

    Add dark mode in Footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,8 +1,6 @@
-
-
 const Footer = () => {
   return (
-    <footer className="text-center text-sm font-medium w-full p-6 md:p-6 bg-black text-white">
+    <footer className="text-center text-sm font-medium w-full p-6 md:p-6 dark:bg-black bg-white dark:text-white text-black">
        © 2024 True Feedback. All rights reserved.
     </footer>
   )
