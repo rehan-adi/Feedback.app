786025720703bb732c60375665d191b2839cca93
Rehan <rehanalire52@gmail.com>
2024-09-10 23:05:55 +0530

    Update profile validation for optional GitHub and Twitter links

--- validation/profile.validation.ts
+++ validation/profile.validation.ts
@@ -1,13 +1,33 @@
 import { z } from "zod";
 
 export const profileValidation = z.object({
-  username: z.string().min(2).max(10),
+  username: z
+  .string()
+  .min(2, { message: "Username must be at least 2 characters." })
+  .max(30, { message: "Username cannot exceed 30 characters." }),
   email: z.string().email(),
   isVerified: z.boolean(),
 });
 
 export const updateProfileValidation = z.object({
-  username: z.string().min(2).max(10),
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." })
+    .max(30, { message: "Username cannot exceed 30 characters." }),
+  githubLink: z
+    .string()
+    .url({ message: "Must be a valid GitHub URL." })
+    .startsWith("https://github.com/", {
+      message: "GitHub link must start with 'https://github.com/'.",
+    })
+    .optional(),
+  twitterLink: z
+    .string()
+    .url({ message: "Must be a valid Twitter URL." })
+    .startsWith("https://twitter.com/", {
+      message: "Twitter link must start with 'https://twitter.com/'.",
+    })
+    .optional(),
 });
 
 export type profileData = z.infer<typeof profileValidation>;
