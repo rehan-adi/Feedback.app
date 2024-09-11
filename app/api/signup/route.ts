b5ad7fee164881e76ca79eb524feed1c0f116aa5
Rehan <rehanalire52@gmail.com>
2024-09-11 18:43:45 +0530

    Implement Redis for verification code storage with 1-hour expiration. And update user signup route

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
+import { redis } from "@/utils/redis";
 import { NextRequest, NextResponse } from "next/server";
 import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
 import { signupValidation } from "@/validation/auth.validation";
@@ -13,36 +14,46 @@ export const POST = async (req: NextRequest) => {
 
     const existingUser = await prisma.user.findUnique({ where: { email } });
 
-    if(existingUser) {
-        return NextResponse.json(
-          { success: false, message: "Email already registered" },
-          { status: 400 }
-        );
+    if (existingUser) {
+      return NextResponse.json(
+        { success: false, message: "Email already registered" },
+        { status: 400 }
+      );
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    
-    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString().padStart(6, '0');
-    const verifyCodeExpiry = new Date(Date.now() + 1000 * 60 * 60 * 24);
+
+    const verifyCode = Math.floor(100000 + Math.random() * 900000)
+      .toString()
+      .padStart(6, "0");
+    const verifyCodeExpiry = 3600;
+
+    await redis.set(`verifyCode:${email}`, verifyCode, "EX", verifyCodeExpiry);
 
     const user = await prisma.user.create({
       data: {
         username,
         email,
         password: hashedPassword,
-        verifyCode,
-        verifyCodeExpiry
       },
     });
 
     await sendVerificationEmail(email, username, verifyCode);
 
-    return NextResponse.json({ success: true, message: "User registered successfully" });
-
+    return NextResponse.json({
+      success: true,
+      message: "User registered successfully",
+    });
   } catch (error: any) {
-    console.error('Signup error:', { message: error.message, stack: error.stack });
+    console.error("Signup error:", {
+      message: error.message,
+      stack: error.stack,
+    });
     return NextResponse.json(
-      { success: false, message: "An error occurred while signing up. Please try again later." },
+      {
+        success: false,
+        message: "An error occurred while signing up. Please try again later.",
+      },
       { status: 500 }
     );
   }
