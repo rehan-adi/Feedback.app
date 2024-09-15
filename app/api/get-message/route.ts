473d00f112178bee70b665c161b0e689cf7c8755
Rehan <rehanalire52@gmail.com>
2024-09-15 19:05:11 +0530

    add force-dynamic rendering to get-message and profile routes

--- app/api/get-message/route.ts
+++ app/api/get-message/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+export const dynamic = 'force-dynamic';
 
 export const GET = async (req: NextRequest) => {
   try {
--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
 
 export const GET = async(req: NextRequest) => {
   try {
