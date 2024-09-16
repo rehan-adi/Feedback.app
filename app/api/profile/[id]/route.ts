6e8a683f33e7b72ef524a06226e87f6c05d014b7
Rehan <rehanalire52@gmail.com>
2024-09-16 23:46:26 +0530

    Refactor GET endpoint to select specific user fields, remove sensitive data, and improve error handling.

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -1,13 +1,44 @@
-import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
-export const  GET = async(req: Request, { params }: { params: { id: string } }) => {
-    
-  const user = await prisma.user.findUnique({
-    where: { id: params.id },
-  });
+export const GET = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: params.id },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        isVerified: true,
+        isAcceptingMessages: true,
+        githubLink: true,
+        twitterLink: true,
+      },
+    });
 
-  if (!user) return NextResponse.json({ error: 'User profile not found' }, { status: 404 });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User profile not found" },
+        { status: 404 }
+      );
+    }
 
-  return NextResponse.json(user);
-}
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.error("Error retrieving user profile:", {
+      message: error.message,
+      stack: error.stack,
+    });
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          "An error occurred while getting user profile. Please try again later.",
+      },
+      { status: 500 }
+    );
+  }
+};
