6fc78040945555b1486cf3e8209310d10e7beaab
Rehan <rehanalire52@gmail.com>
2024-09-18 15:41:49 +0530

    Implement GET request to retrieve user messages

--- app/api/get-message/route.ts
+++ app/api/get-message/route.ts
@@ -1,35 +1,64 @@
-import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 import prisma from "@/lib/prisma";
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const { username } = new URL(req.url);
+    const token = req.cookies.get("token")?.value || "";
 
-    const user = await prisma.user.findFirst({
-      where: { username },
-      include: { messages: true },
+    if (!token) {
+      return NextResponse.json(
+        { success: false, message: "Token is missing" },
+        { status: 401 }
+      );
+    }
+
+    let email: string;
+
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as {
+        email: string;
+      };
+      email = decodedToken.email;
+    } catch (err) {
+      return NextResponse.json(
+        { success: false, message: "Invalid token" },
+        { status: 401 }
+      );
+    }
+
+    if (!email) {
+      return NextResponse.json(
+        { success: false, message: "Email is missing from the token" },
+        { status: 400 }
+      );
+    }
+
+    const userWithMessages = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        messages: {
+          select: {
+            content: true,
+            user: true,
+            createdAt: true,
+          },
+        },
+      },
     });
 
-    if (!user) {
+    if (!userWithMessages) {
       return NextResponse.json(
-        { success: false, message: "User not found" },
+        { success: false, message: "No messages found for the user" },
         { status: 404 }
       );
     }
 
-    if (!user.isAcceptingMessages) {
-      return NextResponse.json(
-        { success: false, message: "User is not accepting messages" },
-        { status: 403 }
-      );
-    }
-
     return NextResponse.json(
-      { success: true, messages: user.messages },
+      { success: true, message: userWithMessages, messages: "Messages retrieved successfully." },
       { status: 200 }
     );
-
   } catch (error: any) {
     console.error("get messages error:", {
       message: error.message,
@@ -38,7 +67,8 @@ export const GET = async (req: NextRequest) => {
     return NextResponse.json(
       {
         success: false,
-        message: "An error occurred while retrieving messages. Please try again later.",
+        message:
+          "An error occurred while retrieving messages. Please try again later.",
       },
       { status: 500 }
     );
